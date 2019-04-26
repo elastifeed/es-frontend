ede933674c166815e220e3f1a7dcdd3efd844cca
Bonnyfication <34171562+Bonnyfication@users.noreply.github.com>
2019-04-26 16:17:45 +0200

    MasterController Class to handle view pathing !important Every Controller should extend MasterController

---
+++ src/main/java/de/htw/saar/frontend/master/MasterController.java
@@ -0,0 +1,29 @@
+package de.htw.saar.frontend.master;
+
+public class MasterController
+{
+    /**
+     * auto generates the path to a view
+     * the view should be located in view/NAME_OF_CONTROLLER/NAME_OF_VIEW
+     * @param name name of the view
+     * @param controller the controller object
+     * @return returns a path to the view
+     */
+    public String view(String name, Object controller)
+    {
+        return "view/" + controller.getClass().getSimpleName().toLowerCase().replace("controller","") + "/" + name + ".xhtml";
+    }
+
+    /**
+     * auto generates the path to a view
+     * the view should be located in view/NAME_OF_CONTROLLER/NAME_OF_VIEW
+     * @param name name of the view
+     * @param controller name of the controller
+     * @return returns a path to the view
+     * @return
+     */
+    public String view(String name, String controller)
+    {
+        return "view/" + controller.toLowerCase() + "/" + name + ".xhtml";
+    }
+}
