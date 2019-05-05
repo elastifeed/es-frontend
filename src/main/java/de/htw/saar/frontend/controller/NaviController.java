53d139a5b4c87f225de4e9abc662add84d417c26
Mohammed <mhmmed93m@gmail.com>
2019-05-05 03:47:36 +0200

    NaviController und navi.xhtml bearbeiten

---
+++ src/main/java/de/htw/saar/frontend/controller/NaviController.java
@@ -0,0 +1,39 @@
+package de.htw.saar.frontend.controller;
+
+
+import de.htw.saar.frontend.TestData.Categorie;
+import de.htw.saar.frontend.service.CategorieService;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import java.util.List;
+
+@ManagedBean (name ="NaviC")
+@ViewScoped
+public class NaviController {
+
+    private CategorieService categorieService=new CategorieService();
+    private List<Categorie> allCategories;
+
+    public List<Categorie> findAllCategories(){
+
+        allCategories=categorieService.findAllCategories();
+        return allCategories;
+    }
+
+    public CategorieService getCategorieService() {
+        return categorieService;
+    }
+
+    public void setCategorieService(CategorieService categorieService) {
+        this.categorieService = categorieService;
+    }
+
+    public void setAllCategories(List<Categorie> allCategories) {
+        this.allCategories = allCategories;
+    }
+
+    public List<Categorie> getAllCategories() {
+        return allCategories;
+    }
+}
