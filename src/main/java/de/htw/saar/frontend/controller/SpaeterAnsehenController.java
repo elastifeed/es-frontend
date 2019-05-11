841215c282663fc27f33f4c741ab1116533fa7b5
airsmart <47214990+airsmart@users.noreply.github.com>
2019-05-11 20:49:38 +0200

    Controller Angelegt: -TimelineController -KategorienController -FavoritenController -SpaeterAnsehenController -ErweiterteSucheController
    Views angelegt:
    zu jedem Controller einen passenden Ordner in View angelegt
    
    In jedem View-Ordner zu jedem Controller index.xhtml angelegt und zum test mit Beispiel-Daten gefüllt.
    
    Jeden Controller zur passender View gemappt.

--- src/main/java/de/htw/saar/frontend/controller/SpaeterAnsehenController.java
+++ src/main/java/de/htw/saar/frontend/controller/SpaeterAnsehenController.java
@@ -1,19 +1,15 @@
 package de.htw.saar.frontend.controller;
 
 import de.htw.saar.frontend.master.MasterController;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class HomeController extends MasterController {
-    @RequestMapping("/")
-    public String main(){
-        return view("index",this);
-    }
-
-    @RequestMapping("/index")
-    public String index() {
+public class SpaeterAnsehenController extends MasterController
+{
+    @RequestMapping("/spaeteransehen")
+    public String main()
+    {
         return view("index",this);
     }
 }
