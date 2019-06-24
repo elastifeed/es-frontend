6a6eb0114a19dabf9fbda242c3954430737b247a
airsmart <47214990+airsmart@users.noreply.github.com>
2019-06-24 19:36:25 +0200

    JavaScript hideTimeline entfernt, da mit data-toggle gelöst

--- src/main/resources/META-INF/resources/js/custom/hideTimeline.js
+++
@@ -1,9 +0,0 @@
-function hideTimeline(){
-    var x = document.getElementById("myTimeline");
-    if (x.style.display === "none"){
-        x.style.display = "block";
-    }
-    else{
-        x.style.display = "none";
-    }
-}
