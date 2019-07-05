778ece9884e6a42c23780f4b23d877773db3475f
Mohammed <mhmmed93m@gmail.com>
2019-07-05 10:31:01 +0200

    Date in Controller erweiterteSuche

--- src/main/java/de/htw/saar/frontend/controller/ErweiterteSucheController.java
+++ src/main/java/de/htw/saar/frontend/controller/ErweiterteSucheController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.inject.Named;
+import java.util.Date;
 
 @Named
 @RequestMapping("/erweitertesuche")
@@ -17,5 +18,23 @@ public class ErweiterteSucheController extends MasterController
     public String index()
     {
         return view("index",this);
+    }
+    private Date startDate;
+    private Date endDate;
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
     }
 }
